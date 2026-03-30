7d4d9baaf31b1e8c1dace2e31149445f61bca7f9
udaya <udayabca2021@gmail.com>
2026-03-31 00:53:18 +0530

    render

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    cors: true,
+    allowedHosts: ["zenzai-page.onrender.com", "localhost"]
+  }
 })
